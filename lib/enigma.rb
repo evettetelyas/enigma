8cc7e5d9242562f56337b0f413c930a852782cc3
Evette Telyas <etelyas@gmail.com>
2019-07-26 12:01:43 -0600

    refactor message

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,7 +3,7 @@ class Enigma < Switchboard
 
   def encrypt(message, board_key = @key, board_date = @date)
     board = Switchboard.new(board_key, board_date)
-    encryption = message.downcase.chars.each_with_index.map do |char, index|
+    encryption = message_data(message).map do |char, index|
       char.tr(alphabet.join, board.shift(index % 4))
     end.join
     output(:encryption, encryption, board_key, board_date)
@@ -11,7 +11,7 @@ class Enigma < Switchboard
 
   def decrypt(message, board_key, board_date = @date)
     board = Switchboard.new(board_key, board_date)
-    decryption = message.downcase.chars.each_with_index.map do |char, index|
+    decryption = message_data(message).map do |char, index|
       char.tr(board.shift(index % 4), alphabet.join)
     end.join
     output(:decryption, decryption, board_key, board_date)
@@ -23,6 +23,10 @@ class Enigma < Switchboard
     blurb[:key] = key
     blurb[:date] = date
     blurb
+  end
+
+  def message_data(message)
+    message.downcase.chars.each_with_index
   end
 
 end
