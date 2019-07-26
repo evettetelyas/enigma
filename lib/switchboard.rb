29463257e8f28eb0ddeeeff0a6a689d260d6160d
Evette Telyas <etelyas@gmail.com>
2019-07-25 23:32:23 -0600

    added DateGenerator class

--- lib/switchboard.rb
+++ lib/switchboard.rb
@@ -1,17 +1,9 @@
 class Switchboard
   attr_reader :date, :key
 
-  def initialize(key = KeyGenerator.new.key, date = default_date)
+  def initialize(key = KeyGenerator.new.key, date = DateGenerator.new.default_date)
     @date = date
     @key = key
-  end
-
-  def default_date
-    array = []
-    configured_date = Date::today.strftime.split("-")
-    year = configured_date[0].chars.pop(2).join
-    array.push(configured_date[2], configured_date[1], year).flatten
-    array.join
   end
 
   def alphabet
