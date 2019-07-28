1f2142bffaa048f9743fbcd83649382c0c712b25
Evette Telyas <etelyas@gmail.com>
2019-07-28 11:22:27 -0600

    added crack to CLI

---
+++ lib/crack.rb
@@ -0,0 +1,16 @@
+require 'date'
+require 'pry'
+require './lib/enigma'
+require './lib/switchboard'
+require './lib/date_generator'
+require './lib/key_generator'
+require './lib/cracker'
+
+enigma = Enigma.new
+message = File.readlines(ARGV[0], "r").join.chomp
+cracked_file = File.open(ARGV[1], "w")
+
+enigma.crack(message, ARGV[2])
+cracked_file.write(enigma.crack(message, ARGV[2])[:decryption])
+
+puts "Created cracked.txt with the key #{enigma.crack(message, ARGV[2])[:key]}, and date #{enigma.crack(message, ARGV[2])[:date]}"
