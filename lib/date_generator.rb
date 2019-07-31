b3f937666de095345406af84623d735b36773244
Evette Telyas <etelyas@gmail.com>
2019-07-31 11:21:10 -0600

    refactor dategen

--- lib/date_generator.rb
+++ lib/date_generator.rb
@@ -1,11 +1,7 @@
 class DateGenerator
 
   def self.default_date
-    array = []
-    configured_date = Date::today.strftime.split("-")
-    year = configured_date[0].chars.pop(2).join
-    array.push(configured_date[2], configured_date[1], year).flatten
-    array.join
+    Date::today.strftime("%d%m%y").delete("/")
   end
 
 end
